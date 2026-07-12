e58d8e1c794ed78efcc73f13df0ac03d6e76ac2f
sahilsingh799 <152429073+sahilsingh799@users.noreply.github.com>
2026-07-12 10:45:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
@@ -2,13 +2,23 @@ class Solution {
   public:
     vector<int> findMajority(vector<int>& arr) {
         // code here
+        // vector<int>ans;
+        // unordered_map<int,int>m;
+        // for(int i=0;i<arr.size();i++){
+        //     m[arr[i]]++;
+        //     if(m[arr[i]]==arr.size()/3+1){
+        //         ans.push_back(arr[i]);
+        //     }
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
         vector<int>ans;
         unordered_map<int,int>m;
-        for(int i=0;i<arr.size();i++){
-            m[arr[i]]++;
-            if(m[arr[i]]==arr.size()/3+1){
-                ans.push_back(arr[i]);
-            }
+        for(auto x: arr){
+            m[x]++;
+        }
+        for(auto a: m){
+            if(a.second>arr.size()/3) ans.push_back(a.first);
         }
         sort(ans.begin(),ans.end());
         return ans;
